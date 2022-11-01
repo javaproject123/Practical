d1ab8744fb8ea28f5b92c60486bddbcfe5601256
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-11-01 13:13:34 +0530

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Array2D {
+	public static void main(String args[]) {
+		int arr[][]= {{2,5,6}, {4,5,6}, {7,8,9}};
+		for (int i=0;i<3;i++){
+			for (int j=0;j<3;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
