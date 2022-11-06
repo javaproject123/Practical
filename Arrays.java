e08d375d84b99f3ea8d125347b69622c9a07c6b0
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-11-06 09:33:24 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Arrays {
+
+	public static void main(String[] args) {
+		int[]marks = new int[5];
+		marks[0]=97; // math
+		marks[1]=87; // Eng
+		marks[2]=69; // phy
+		marks[3]=54; // chem
+		marks[4]=45; // hist
+		//System.out.println(marks[0]);
+		//System.out.println(marks[1]);
+		//System.out.println(marks[2]);
+		//System.out.println(marks[3]);
+		//System.out.println(marks[4]);
+         for(int i=0;i<5;i++) {
+        	 System.out.println(marks[i]);
+         }
+	}
+
+}
